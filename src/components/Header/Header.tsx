bd1c90b348f5502ab2e561cc633e1d1fef68c248
SwannyAlves <swannykathllen@gmail.com>
2024-09-07 16:38:09 -0300

    style: add padding in Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,12 +4,12 @@ interface HeaderProps {
 
 export const Header = ({ children }: HeaderProps) => {
   return (
-    <header className="flex items-center justify-center bg-[#1D1D1D] py-[22px]">
+    <header className="flex items-center justify-center bg-[#1D1D1D] py-[22px] px-4 xl:px-0 ">
       <div className="flex flex-col md:flex-row md:justify-start items-start md:items-center max-w-[1152px] w-full gap-[20px] md:gap-[54px]">
         <h1 className="text-xl font-black bg-clip-text text-transparent bg-gradient-to-b from-[#00F2D5] to-[#AD00FF]">
           MUSE.ai
         </h1>
-        <div className="text-left md:text-center md:mt-0 w-full md:w-auto">
+        <div className="text-left md:text-center md:mt-0 w-full">
           {children}
         </div>
       </div>
