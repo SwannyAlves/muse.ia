5c6a6f353c5a6a767b8d38d6eaba39acb3d14559
SwannyAlves <swannykathllen@gmail.com>
2024-09-08 11:15:39 -0300

    feat: add handleKeyDown in TextInput

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -4,7 +4,7 @@ import ic_search from "../../../public/assets/icons/ic-search.svg"
 
 interface TextInputProps {
   onSearch?: (value: string) => void
-  placeholder?: string
+  placeholder: string
   suggestions: string[]
   value: string
   onChange: (value: string) => void
@@ -30,10 +30,9 @@ export const TextInput = ({
     (e: React.ChangeEvent<HTMLInputElement>) => {
       const newValue = e.target.value
       onChange(newValue)
-      onSearch?.(newValue)
       setShowSuggestions(true)
     },
-    [onChange, onSearch]
+    [onChange]
   )
 
   const handleOnSelectSuggestion = useCallback(
@@ -53,9 +52,18 @@ export const TextInput = ({
     setShowSuggestions(true)
   }, [])
 
+  const handleKeyDown = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === "Enter" && onSearch) {
+        onSearch(value)
+      }
+    },
+    [onSearch, value]
+  )
+
   return (
     <div className="relative w-full">
-      <div className="flex items-center max-w-[359px] w-full gap-2 p-2 rounded-lg bg-[#262626] focus-within:bg-[#404040] transition-colors duration-300">
+      <div className="flex items-center max-w-[359px] w-full gap-2 px-2 py-[7px] rounded-lg bg-[#262626] focus-within:bg-[#404040] transition-colors duration-300">
         <Image src={ic_search} alt="Search Icon" width={20} height={20} />
         <input
           type="text"
@@ -64,7 +72,8 @@ export const TextInput = ({
           onChange={handleOnChange}
           onFocus={handleOnFocus}
           onBlur={handleOnBlur}
-          className="w-full text-white text-base font-[500] bg-[#262626] focus:bg-[#404040] transition-colors duration-300 outline-none placeholder:text-white"
+          onKeyDown={handleKeyDown}
+          className="w-full text-white text-base font-normal leading[19.2px] bg-[#262626] focus:bg-[#404040] transition-colors duration-300 outline-none placeholder:text-white"
         />
       </div>
 
@@ -72,13 +81,13 @@ export const TextInput = ({
         <ul className="absolute max-w-[359px] w-full bg-[#2D2D2D] rounded-[5px] border-none p-[14px] mt-[6px] z-10">
           {filterSuggestions.map((suggestion, index) => (
             <li
-              className="cursor-pointer text-start"
+              className="cursor-pointer text-start text-[#D1D1D1]"
               key={index}
               onMouseDown={() => handleOnSelectSuggestion(suggestion)}
             >
               {suggestion}
               {index !== filterSuggestions.length - 1 && (
-                <hr className="my-[14px]  border border-[#545454]" />
+                <hr className="my-[14px] border border-[#545454]" />
               )}
             </li>
           ))}
